20011e6d59bc4c648280b43e9b004f47dfd14166
EstebanDev <guachichullcaesteban@gmail.com>
2025-05-21 07:51:54 -0500

    Informacion de servicios separada del db

---
+++ src/data/services.js
@@ -0,0 +1,34 @@
+export const services = [
+  {
+    id: 1,
+    name_service: "Servicio a la carta",
+    description_service:
+      "Los clientes eligen platos individuales desde el menú.",
+    url_image:
+      "https://res.cloudinary.com/di2fo6cj8/image/upload/v1747798930/servicio-carta_ccntug.webp",
+  },
+  {
+    id: 2,
+    name_service: "Entrega a domicilio",
+    description_service:
+      "Llevamos los pedidos directamente al domicilio del cliente.",
+    url_image:
+      "https://res.cloudinary.com/di2fo6cj8/image/upload/v1747798931/entrega-domicilio_nzk5il.webp",
+  },
+  {
+    id: 3,
+    name_service: "Reservaciones y eventos",
+    description_service:
+      "Espacios disponibles para eventos especiales y reuniones.",
+    url_image:
+      "https://res.cloudinary.com/di2fo6cj8/image/upload/v1747798930/reservaciones_ug8i9r.webp",
+  },
+  {
+    id: 4,
+    name_service: "Catering para empresas",
+    description_service:
+      "Servicio de comida para reuniones y eventos corporativos.",
+    url_image:
+      "https://res.cloudinary.com/di2fo6cj8/image/upload/v1747798930/eventos-catering_iwdehe.webp",
+  },
+];
